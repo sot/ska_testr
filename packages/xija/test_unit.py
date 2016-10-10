685c441bd574ec88c76ec93495b666500c25d67a
Tom Aldcroft <taldcroft@gmail.com>
2016-10-10 17:22:30 -0400

    Fix xija unit test

--- packages/xija/test_unit.py
+++ packages/xija/test_unit.py
@@ -1,4 +1,4 @@
 import xija
-n_fail = xija.test('-v', '-k test_data_types')
+n_fail = xija.test('-v', '-s', '-k not test_minusz')
 if n_fail > 0:
     raise ValueError(str(n_fail) + ' test failures')
