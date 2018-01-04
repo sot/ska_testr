0a5ceb63d6db02da34572acc9a3420d9ba03bc11
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-01-04 10:39:50 -0500

    Ignore mica expected warning in regress test on multi ai

--- packages/mica/post_check_logs.py
+++ packages/mica/post_check_logs.py
@@ -2,4 +2,5 @@ from testr.packages import check_files
 
 check_files('test_*.log', ['warning', 'error'],
             allows=['did not parse as fits unit',
-                    'specified but multiple tables'])
+                    'specified but multiple tables',
+                    'has 2 aspect intervals'])
