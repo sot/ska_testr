dd0c209d76e30e7959b391709561cf962ef40bfb
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-07-25 22:02:46 -0400

    Update ACIS checker tests
    These use the newer ACIS state builder by default and work only on
    newer directories and out of the ACIS load review area.

--- packages/dea_check/post_check_logs.py
+++ packages/dea_check/post_check_logs.py
@@ -1,4 +1,6 @@
 from testr.packages import check_files
 
 check_files('test_*.log', ['warning', 'error'],
-            allows=['99% quantile value of', 'in output at out'])
+            allows=['1% quantile value of',
+                    '99% quantile value of',
+                    'in output at out'])
--- packages/dpa_check/post_check_logs.py
+++ packages/dpa_check/post_check_logs.py
@@ -1,4 +1,8 @@
 from testr.packages import check_files
 
 check_files('test_*.log', ['warning', 'error'],
-            allows=['99% quantile value of', 'in output at out'])
+            allows=['1% quantile value of',
+                    '1dpamzt violates planning limit of 35.50 deg',
+                    '50% quantile value of',
+                    '99% quantile value of',
+                    'in output at out'])
