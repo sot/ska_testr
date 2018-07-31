8c1240e5181881579c4f498a2f28f4538bdcfac4
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-07-31 16:34:29 -0400

    Allow AstropyDep warning from acis checkers

--- packages/dea_check/post_check_logs.py
+++ packages/dea_check/post_check_logs.py
@@ -3,4 +3,5 @@ from testr.packages import check_files
 check_files('test_*.log', ['warning', 'error'],
             allows=['1% quantile value of',
                     '99% quantile value of',
-                    'in output at out'])
+                    'in output at out',
+                    'AstropyDeprecationWarning: out/states.dat already exists.'])
--- packages/dpa_check/post_check_logs.py
+++ packages/dpa_check/post_check_logs.py
@@ -5,4 +5,5 @@ check_files('test_*.log', ['warning', 'error'],
                     '1dpamzt violates planning limit of 35.50 deg',
                     '50% quantile value of',
                     '99% quantile value of',
-                    'in output at out'])
+                    'in output at out',
+                    'AstropyDeprecationWarning: out/states.dat already exists.'])
