fa82ad925af2bcf21a22e0b9840d97d67144015a
Jean Connelly <jconnelly@cfa.harvard.edu>
2018-07-31 07:44:07 -0400

    Add missing comma to mica check_files

--- packages/mica/post_check_logs.py
+++ packages/mica/post_check_logs.py
@@ -3,5 +3,5 @@ from testr.packages import check_files
 check_files('test_*.log', ['warning', 'error'],
             allows=['did not parse as fits unit',
                     'specified but multiple tables',
-                    'has 2 aspect intervals'
+                    'has 2 aspect intervals',
                     'NaturalNameWarning:'])
