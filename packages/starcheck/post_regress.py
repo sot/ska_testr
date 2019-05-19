127c25d53c6e85d94ee8d200bff664044f506372
Jean Connelly <jconnelly@cfa.harvard.edu>
2019-05-19 09:18:50 -0400

    Remove pngs from regress (don't work in py3 anyway)

--- packages/starcheck/post_regress.py
+++ packages/starcheck/post_regress.py
@@ -1,8 +1,6 @@
 from testr.packages import make_regress_files
 
 regress_files = ['starcheck.txt',
-                 'starcheck/ccd_temperature.png',
-                 'starcheck/stars_18125.png',
                  'starcheck/pcad_att_check.txt']
 
 clean = {'starcheck.txt': [(r'\s*Run on.*[\n\r]*', '')]}
