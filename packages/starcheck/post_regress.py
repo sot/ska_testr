2a92059452ed7e388329c0bd52a2774af67ffc1c
Jean Connelly <jconnelly@cfa.harvard.edu>
2019-05-19 09:27:49 -0400

    Remove SKA path from starcheck outputs

--- packages/starcheck/post_regress.py
+++ packages/starcheck/post_regress.py
@@ -1,8 +1,11 @@
+import os
 from testr.packages import make_regress_files
 
 regress_files = ['starcheck.txt',
                  'starcheck/pcad_att_check.txt']
 
-clean = {'starcheck.txt': [(r'\s*Run on.*[\n\r]*', '')]}
+clean = {'starcheck.txt': [(r'\s*Run on.*[\n\r]*', ''),
+                           (os.environ['SKA'], '')],
+         'starcheck/pcad_att_check.txt': [(os.environ['SKA'], '')]}
 
 make_regress_files(regress_files, clean=clean)
