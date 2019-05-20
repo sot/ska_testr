5102971a07c351b397287c3ecd9d235891756d5f
Jean Connelly <jconnelly@cfa.harvard.edu>
2019-05-19 22:59:42 -0400

    Clean up ACIS regress output noise

--- packages/acisfp_check/post_regress.py
+++ packages/acisfp_check/post_regress.py
@@ -6,8 +6,9 @@ regress_files = ['out/index.rst',
                  'out/states.dat',
                  'out/temperatures.dat']
 
-clean = {'out/index.rst': [(r'^Run time.*', '')],
-         'out/run.dat': [(r'#.*py run at.*', ''),
+clean = {'out/index.rst': [(r'^Run time.*', ''),
+                           ('Load directory.*', '')],
+         'out/run.dat': [(r'#.*run at.*', ''),
                          (os.environ['SKA'], '')]}
 
 make_regress_files(regress_files, clean=clean)
