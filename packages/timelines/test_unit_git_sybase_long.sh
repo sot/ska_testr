9417b450445c831b046e1ff120edb6b841958eb1
Tom Aldcroft <taldcroft@gmail.com>
2020-11-01 08:12:25 -0500

    Shiny cleanup

--- packages/timelines/test_unit_git_sybase_long.sh
+++
@@ -1,6 +0,0 @@
-# Command states scripts and module already installed in skadev
-GIT=`PATH=/usr/bin:$PATH which git`
-$GIT clone ${TESTR_PACKAGES_REPO}/${TESTR_PACKAGE}
-cd ${TESTR_PACKAGE}
-$GIT checkout master
-# nosetests timelines_test.py
