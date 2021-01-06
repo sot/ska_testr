444010cacddbde3626580feb146e2d02df3b6139
Jean Connelly <jconnelly@cfa.harvard.edu>
2021-01-06 18:12:58 -0500

    Ignore dpa zero-fep warning for test success

--- packages/dpa_check/post_check_logs.py
+++ packages/dpa_check/post_check_logs.py
@@ -5,6 +5,7 @@ check_files('test_*.log', ['warning', 'error'],
                     '1dpamzt violates planning limit of 35.50 deg',
                     '50% quantile value of',
                     '99% quantile value of',
+                    '1dpamzt violates zero-feps limit of 12.00 degC',
                     'validation warning\(s\) in output',
                     'AstropyDeprecationWarning: out/states.dat already exists.',
                     'AstropyDeprecationWarning: headout/states.dat already exists.'])
