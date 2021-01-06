d0c02457fcedc8175bcd3db3e70640c3a9821e93
Jean Connelly <jconnelly@cfa.harvard.edu>
2021-01-06 18:09:43 -0500

    Ignore ErfaWarnings from astropy in acis checkers

--- packages/psmc_check/post_check_logs.py
+++ packages/psmc_check/post_check_logs.py
@@ -6,4 +6,6 @@ check_files('test_*.log', ['warning', 'error'],
                     '99% quantile value of',
                     'validation warning\(s\) in output',
                     'AstropyDeprecationWarning: out/states.dat already exists.',
-                    'AstropyDeprecationWarning: headout/states.dat already exists.'])
+                    'AstropyDeprecationWarning: headout/states.dat already exists.',
+                    'ErfaWarning: ERFA function "dtf2d" yielded 2 of "dubious year',
+                    'ErfaWarning: ERFA function "utctai" yielded 2 of "dubious year'])
