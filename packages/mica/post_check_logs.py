809d1642c4c78ebe35c2c82d054e088d672292d4
Jean Connelly <jconnelly@cfa.harvard.edu>
2021-01-06 22:14:14 -0500

    Ignore mica sybase warning if no access file

--- packages/mica/post_check_logs.py
+++ packages/mica/post_check_logs.py
@@ -1,7 +1,19 @@
+import os
+from pathlib import Path
+import getpass
 from testr.packages import check_files
 
+
+allows = ['did not parse as fits unit',
+          'specified but multiple tables',
+          'has 2 aspect intervals',
+          'NaturalNameWarning:']
+
+# If the running user does not have a Sybase V&V access file, ignore report test fail.
+user = getpass.getuser()
+auth_file = Path(os.environ['SKA']) / 'data' / 'aspect_authorization' / f'sqlsao-axafvv-{user}'
+if not auth_file.exists():
+    allows.append('UserWarning: On HEAD but no sybase access.')
+
 check_files('test_*.log', ['warning', 'error'],
-            allows=['did not parse as fits unit',
-                    'specified but multiple tables',
-                    'has 2 aspect intervals',
-                    'NaturalNameWarning:'])
+            allows=allows)
