a866e623cb76a8bdede021bb01bb9a5c0766b562
Jean Connelly <jconnelly@cfa.harvard.edu>
2021-11-30 09:31:15 -0500

    Change sql state-builder to kadi

--- packages/acisfp_check/test_regress.sh
+++ packages/acisfp_check/test_regress.sh
@@ -2,5 +2,5 @@ acisfp_check \
    --outdir=out \
    --oflsdir=${SKA}/data/ska_testr/test_loads/2019/MAY2019/oflsa \
    --nlet_file=${SKA}/data/ska_testr/test_loads/2019/MAY2019/oflsa/NonLoadTrackedEvents.txt \
-   --state-builder=sql \
+   --state-builder=kadi \
    --run-start=2019:135
