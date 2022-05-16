3b5241278994387ec67604d7bc60d0a3fc32be74
Jean Connelly <jconnelly@cfa.harvard.edu>
2022-05-16 11:24:30 -0400

    Allow 50% quantile warn in dea regress output

--- packages/dea_check/post_check_logs.py
+++ packages/dea_check/post_check_logs.py
@@ -3,6 +3,7 @@ from testr.packages import check_files
 check_files('test_*.log', ['warning', 'error'],
             allows=['1% quantile value of',
                     '99% quantile value of',
+                    '50% quantile value of',
                     'validation warning\(s\) in output',
                     'AstropyDeprecationWarning: headout/states.dat already exists.',
                     'AstropyDeprecationWarning: out/states.dat already exists.'])
