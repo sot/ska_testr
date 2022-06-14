c07896adfb3c9a01d1e17c6fde1b41b0aa2f46ba
Jean Connelly <jconnelly@cfa.harvard.edu>
2022-06-14 08:38:16 -0400

    Remove hard limits from check_files allows

--- packages/dpa_check/post_check_logs.py
+++ packages/dpa_check/post_check_logs.py
@@ -2,10 +2,10 @@ from testr.packages import check_files
 
 check_files('test_*.log', ['warning', 'error'],
             allows=['1% quantile value of',
-                    '1dpamzt violates planning limit of 35.50 deg',
+                    '1dpamzt violates planning limit',
                     '50% quantile value of',
                     '99% quantile value of',
-                    '1dpamzt violates zero-feps limit of 12.00 degC',
+                    '1dpamzt violates zero-feps limit',
                     'validation warning\(s\) in output',
                     'AstropyDeprecationWarning: out/states.dat already exists.',
                     'AstropyDeprecationWarning: headout/states.dat already exists.'])
