8567f5168f4225ef46ffd905704eb9ef3cd546d2
Jean Connelly <jconnelly@cfa.harvard.edu>
2022-09-28 14:38:35 -0400

    Trim down test 'allows' and add the 'no starcat' allow

--- packages/kadi/post_check_logs.py
+++ packages/kadi/post_check_logs.py
@@ -1,11 +1,4 @@
 from testr.packages import check_files
 
 check_files('test_*.log', ['warning', 'error'],
-            allows=[r'/kadi/settings.py:\d\d: UserWarning:',
-                    r'warnings.warn\(message\)',
-                    'Unable to change file mode',
-                    'unable to get COBSRQID',
-                    'alter_validators_add_error_messages',
-                    'dropping state because of insufficent event time pad',
-                    'negative event duration',
-                    'Coarse OBC'])
+            allows=['no starcat for obsid 0'])
