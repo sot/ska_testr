a04504ba7d3b03250255810545783c8a6ea9582f
Javier Gonzalez <javierggt@yahoo.com>
2023-01-09 11:59:13 -0500

    add an exception to pattern matching in starcheck/post_check_logs.py

--- packages/starcheck/post_check_logs.py
+++ packages/starcheck/post_check_logs.py
@@ -1,3 +1,7 @@
 from testr.packages import check_files
 
-check_files('test_*.log', ['warning', 'error'])
+check_files(
+    'test_*.log',
+    ['warning', 'error'],
+    allows=["Reading Maneuver Error file"]
+)
