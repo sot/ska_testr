765e870eae97937a67289fc742f20746c726d3e4
Jean Connelly <jconnelly@cfa.harvard.edu>
2023-04-24 13:49:03 -0400

    Ignore error in name of a test

--- packages/cheta/post_check_logs.py
+++ packages/cheta/post_check_logs.py
@@ -3,4 +3,5 @@ from testr.packages import check_files
 check_files('test_*.log', ['warning', 'error'],
             allows=['WARNING.+because colnames',
                     'WARNING.+made new file',
-                    'WARNING.+Unexpected null file'])
+                    'WARNING.+Unexpected null file',
+                    'error.+one_value_error PASSED'])
