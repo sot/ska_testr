008d491df73d2e1a7a227f6c5ccbb9516491775a
Javier Gonzalez <javierggt@yahoo.com>
2023-07-12 16:01:46 -0400

    ignore appearance of test_clip_warning PASS

--- packages/chandra_aca/post_check_logs.py
+++ packages/chandra_aca/post_check_logs.py
@@ -1,5 +1,10 @@
 from testr.packages import check_files
 
-check_files('test_*.log', ['warning', 'error'],
-            allows=['using `oa_ndim == 0` when `op_axes` is NULL is deprecated.',
-                    'mica.archive.aca_dark.dark_model is deprecated.'])
+check_files(
+    'test_*.log', ['warning', 'error'],
+    allows=[
+        'using `oa_ndim == 0` when `op_axes` is NULL is deprecated.',
+        'mica.archive.aca_dark.dark_model is deprecated.',
+        'test_clip_warning PASSED',
+    ]
+)
