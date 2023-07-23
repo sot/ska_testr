c5ef14fbbd2a796a5acf9e9a8ac0cc069a9d1143
Jean Connelly <jconnelly@cfa.harvard.edu>
2023-07-23 11:35:17 -0400

    Move Ska.Shell tests into ska_shell

--- packages/Ska.Shell/post_check_logs.py
+++
@@ -1,4 +0,0 @@
-from testr.packages import check_files
-
-check_files('test_*.log', ['warning', 'error'],
-            allows=[r'test_[a-z]+_error'])
---
+++ packages/ska_shell/test_namespace.py
@@ -0,0 +1,2 @@
+import Ska.Shell
+Ska.Shell.test(verbose=True, raise_exception=True, show_output=True)
